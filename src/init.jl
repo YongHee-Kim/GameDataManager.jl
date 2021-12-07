2fc2a78ec4cc5746baac3020e3c9f66e55c7033e
YongHeeKim <masterplanf@gmail.com>
2021-12-07 17:14:21 +0900

    look for config from init

--- src/init.jl
+++ src/init.jl
@@ -1,6 +1,11 @@
-const GAMEENV = Dict{String,Any}()
+const GAMEENV = Dict{String,String}()
 const CACHE = Dict{String,Any}(
-        "config" => missing,
-        "validation" => true,
-        "tables" => Dict{Symbol,Any}(),
-        "tablesschema" => Dict())
+"config" => missing,
+"tables" => Dict{Symbol,Any}(),
+"tablesschema" => Dict())
+
+function __init__()
+    if isfile(joinpath(pwd(), "config.json"))
+        init_project(pwd())
+    end
+end
