4704a531eb8409d6dfd223f54db5bddba402ebd9
YongheeKim <masterplanf@gmail.com>
2024-03-30 21:17:11 +0900

    clean doc branch

--- src/init.jl
+++
@@ -1,9 +0,0 @@
-const GAMEENV = Dict{String,String}()
-const CACHE = Dict{String,Any}(
-"config" => missing)
-
-function __init__()
-    if isfile(joinpath(pwd(), "config.json"))
-        init_project(pwd())
-    end
-end
